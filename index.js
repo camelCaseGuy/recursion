ac156bd857d8c61401e5b5985cb0feb3f0763352
Ryan Hanley <camelCaseGuy@users.noreply.github.com>
2022-09-16 13:00:44 -0500

    added fibsRec and styling

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const fibs = (num) => {
-  const arr = [0,1];
-  while(arr.slice(-1) < num) {
+  const arr = [0, 1];
+  while (arr.slice(-1) < num) {
     const add = Number(arr.slice(-2, -1)) + Number(arr.slice(-1));
     arr.push(add);
   }
   return arr;
 }
 
-console.log(fibs(20));
+console.log('fibs:   ', fibs(20));
+
+
+const fibsRec = (num, arr) => {
+  let tempArr = [];
+  if (arr === undefined) {
+    tempArr = [0, 1];
+  } else {
+    tempArr = arr;
+  }
+  if (tempArr.slice(-1) < num) {
+    tempArr.push(Number(tempArr.slice(-2, -1)) + Number(tempArr.slice(-1)));
+    fibsRec(num, tempArr);
+  }
+  
+  return tempArr;
+}
+
+console.log('fibsRec:', fibsRec(20));
